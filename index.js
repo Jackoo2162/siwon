088d368741974333a3f4d6cd6d493e3ed9acadb6
Jackoo2162 <35918848+Jackoo2162@users.noreply.github.com>
2018-02-04 17:02:33 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+
+
+bot.on('message', message => {
+  if (message.content === 'Kawai') {
+   // message.reply('pong');
+    message.channel.sendMessage('hi');
+  }
+});
+
+bot.on("ready", () => {
+  bot.user.setPresence({game: {name:"BUYING BLACK SUITS!"}})
+  console.log('I have jackoo: {SERVERNAME}')
+})
+
+bot.on('ready', ()=>{
+  console.log(`Logged in as ${bot.user.tag}!`);
+  voiceChannel = bot.guilds.find('name', 'DREAMENERG3T1CER').channels.find('name', 'SIWON');
+  voiceChannel.join().then(()=>{
+      console.log(`Connected to ${voiceChannel.name}!`);
+  });
+});
+
+
+bot.on('message', message=>{
+  if (message.content.startsWith('Hi')){
+      message.author.send('Hi, how can I help you?');
+  }
+});
+
+// Create an event listener for new guild members
+bot.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'general');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+
+bot.login('NDA4NTkwODEzMzk2OTkyMDAw.DVSRXQ.GViw06G0EBTH3WyGcXvmwvMsi2g');
